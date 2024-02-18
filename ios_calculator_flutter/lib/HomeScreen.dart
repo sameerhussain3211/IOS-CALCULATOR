4983fd7b4bbcb919335f35a45f1c670518f63a2a
sameerhussain3211 <sameerhussain44332211@gmail.com>
2024-02-19 00:38:26 +0500

    second edit

--- ios_calculator_flutter/lib/HomeScreen.dart
+++ ios_calculator_flutter/lib/HomeScreen.dart
@@ -11,15 +11,104 @@ class homeScreen extends StatefulWidget {
 
 class _loginScreenState extends State<homeScreen> {
   @override
+  double counter = 0;
+  double num1 = 0;
+  String number1 = '';
+  String number2 = '';
+  double num2 = 0;
+  String symbol = '';
+  int lengthofcounter = 0;
+
+  void _reset() {
+    setState(() {
+      counter = 0;
+      num1 = 0;
+      num2 = 0;
+      number1 = '';
+      number2 = '';
+      symbol = '';
+      lengthofcounter = 0;
+    });
+  }
+
+  void _reset2() {
+    // counter = 0;
+    num1 = counter;
+    number1 = '';
+    number2 = '';
+    symbol = '';
+  }
+
+  void _add() {
+    setState(() {
+      counter = num1 + num2;
+    });
+  }
+
+  void _sub() {
+    setState(() {
+      counter = num1 - num2;
+    });
+  }
+
+  void _multiply() {
+    setState(() {
+      counter = num1 * num2;
+    });
+  }
+
+  void divide() {
+    setState(() {
+      counter = num1 / num2;
+    });
+  }
+
+  void percent() {
+    setState(() {
+      counter = counter / 100;
+    });
+  }
+
+  void _negation() {
+    setState(() {
+      counter = counter * -1;
+    });
+  }
+
+  void _resultShow() {
+    setState(() {
+      counter = num1 + num2;
+    });
+  }
+
+  void _refresh() {
+    setState(() {});
+  }
+ 
+
   Widget build(BuildContext context) {
+     
     return Scaffold(
       backgroundColor: const Color.fromARGB(243, 0, 0, 0),
       body: SafeArea(
           child: Column(
         children: [
-          Container(
-            color: Colors.green,
-            height: 300,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 300,
+              child: Container(
+                alignment: Alignment.bottomRight,
+                child: Text(
+                  "$counter",
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 75,
+                      fontWeight: FontWeight.normal),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+            ),
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
@@ -33,7 +122,10 @@ class _loginScreenState extends State<homeScreen> {
                     child: CalButtonTwoLetter(
                         buttonText: "AC",
                         buttonLength: 75,
-                        onPressedCallback: () {},
+                        onPressedCallback: () {
+                          counter = 0;
+                          _reset();
+                        },
                         colorButton: Colors.grey.shade400),
                   ),
                 ),
@@ -45,7 +137,9 @@ class _loginScreenState extends State<homeScreen> {
                     child: CalButtonTwoLetter(
                         buttonText: "+/-",
                         buttonLength: 75,
-                        onPressedCallback: () {},
+                        onPressedCallback: () {
+                          _negation();
+                        },
                         colorButton: Colors.grey.shade400),
                   ),
                 ),
@@ -57,19 +151,24 @@ class _loginScreenState extends State<homeScreen> {
                     child: CalButtonTwoLetter(
                         buttonText: "% ",
                         buttonLength: 75,
-                        onPressedCallback: () {},
+                        onPressedCallback: () {
+                          percent();
+                        },
                         colorButton: Colors.grey.shade400),
                   ),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Container(
+                  child: SizedBox(
                     height: 75,
                     child: cal_button(
                       buttonText: "÷",
                       buttonLength: 75,
                       color_button: Colors.amber,
-                      onPressedCallback: () {},
+                      onPressedCallback: () {
+                        symbol = '÷';
+                        number1 = number1 + symbol;
+                      },
                     ),
                   ),
                 ),
@@ -82,13 +181,27 @@ class _loginScreenState extends State<homeScreen> {
               children: [
                 Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Container(
+                  child: SizedBox(
                     height: 75,
                     child: cal_button(
                       buttonText: "7",
                       buttonLength: 75,
                       color_button: const Color.fromARGB(42, 255, 255, 255),
-                      onPressedCallback: () {},
+                      onPressedCallback: () {
+                        if (number1.endsWith('÷') ||
+                            number1.endsWith('+') ||
+                            number1.endsWith('x') ||
+                            number1.endsWith('-')) {
+                          number2 = number2 + '7';
+                          num2 = double.parse(number2);
+                          counter = double.parse(number2);
+                        } else {
+                          number1 = number1 + '7';
+                          num1 = double.parse(number1);
+                          counter = double.parse(number1);
+                        }
+                        _refresh();
+                      },
                     ),
                   ),
                 ),
@@ -100,7 +213,21 @@ class _loginScreenState extends State<homeScreen> {
                       buttonText: "8",
                       buttonLength: 75,
                       color_button: const Color.fromARGB(42, 255, 255, 255),
-                      onPressedCallback: () {},
+                      onPressedCallback: () {
+                        if (number1.endsWith('÷') ||
+                            number1.endsWith('+') ||
+                            number1.endsWith('x') ||
+                            number1.endsWith('-')) {
+                          number2 = number2 + '8';
+                          num2 = double.parse(number2);
+                          counter = double.parse(number2);
+                        } else {
+                          number1 = number1 + '8';
+                          num1 = double.parse(number1);
+                          counter = double.parse(number1);
+                        }
+                        _refresh();
+                      },
                     ),
                   ),
                 ),
@@ -112,7 +239,21 @@ class _loginScreenState extends State<homeScreen> {
                       buttonText: "9",
                       buttonLength: 75,
                       color_button: const Color.fromARGB(42, 255, 255, 255),
-                      onPressedCallback: () {},
+                      onPressedCallback: () {
+                        if (number1.endsWith('÷') ||
+                            number1.endsWith('+') ||
+                            number1.endsWith('x') ||
+                            number1.endsWith('-')) {
+                          number2 = number2 + '9';
+                          num2 = double.parse(number2);
+                          counter = double.parse(number2);
+                        } else {
+                          number1 = number1 + '9';
+                          num1 = double.parse(number1);
+                          counter = double.parse(number1);
+                        }
+                        _refresh();
+                      },
                     ),
                   ),
                 ),
@@ -124,7 +265,10 @@ class _loginScreenState extends State<homeScreen> {
                       buttonText: "x",
                       buttonLength: 75,
                       color_button: Colors.amber,
-                      onPressedCallback: () {},
+                      onPressedCallback: () {
+                        symbol = 'x';
+                        number1 = number1 + symbol;
+                      },
                     ),
                   ),
                 ),
@@ -143,7 +287,21 @@ class _loginScreenState extends State<homeScreen> {
                       buttonText: "4",
                       buttonLength: 75,
                       color_button: const Color.fromARGB(42, 255, 255, 255),
-                      onPressedCallback: () {},
+                      onPressedCallback: () {
+                        if (number1.endsWith('÷') ||
+                            number1.endsWith('+') ||
+                            number1.endsWith('x') ||
+                            number1.endsWith('-')) {
+                          number2 = number2 + '4';
+                          num2 = double.parse(number2);
+                          counter = double.parse(number2);
+                        } else {
+                          number1 = number1 + '4';
+                          num1 = double.parse(number1);
+                          counter = double.parse(number1);
+                        }
+                        _refresh();
+                      },
                     ),
                   ),
                 ),
@@ -155,7 +313,21 @@ class _loginScreenState extends State<homeScreen> {
                       buttonText: "5",
                       buttonLength: 75,
                       color_button: const Color.fromARGB(42, 255, 255, 255),
-                      onPressedCallback: () {},
+                      onPressedCallback: () {
+                        if (number1.endsWith('÷') ||
+                            number1.endsWith('+') ||
+                            number1.endsWith('x') ||
+                            number1.endsWith('-')) {
+                          number2 = number2 + '5';
+                          num2 = double.parse(number2);
+                          counter = double.parse(number2);
+                        } else {
+                          number1 = number1 + '5';
+                          num1 = double.parse(number1);
+                          counter = double.parse(number1);
+                        }
+                        _refresh();
+                      },
                     ),
                   ),
                 ),
@@ -167,7 +339,21 @@ class _loginScreenState extends State<homeScreen> {
                       buttonText: "6",
                       buttonLength: 75,
                       color_button: const Color.fromARGB(42, 255, 255, 255),
-                      onPressedCallback: () {},
+                      onPressedCallback: () {
+                        if (number1.endsWith('÷') ||
+                            number1.endsWith('+') ||
+                            number1.endsWith('x') ||
+                            number1.endsWith('-')) {
+                          number2 = number2 + '6';
+                          num2 = double.parse(number2);
+                          counter = double.parse(number2);
+                        } else {
+                          number1 = number1 + '6';
+                          num1 = double.parse(number1);
+                          counter = double.parse(number1);
+                        }
+                        _refresh();
+                      },
                     ),
                   ),
                 ),
@@ -179,7 +365,10 @@ class _loginScreenState extends State<homeScreen> {
                       buttonText: "-",
                       buttonLength: 75,
                       color_button: Colors.amber,
-                      onPressedCallback: () {},
+                      onPressedCallback: () {
+                        symbol = '-';
+                        number1 = number1 + symbol;
+                      },
                     ),
                   ),
                 ),
@@ -198,7 +387,21 @@ class _loginScreenState extends State<homeScreen> {
                       buttonText: "1",
                       buttonLength: 75,
                       color_button: const Color.fromARGB(42, 255, 255, 255),
-                      onPressedCallback: () {},
+                      onPressedCallback: () {
+                        if (number1.endsWith('÷') ||
+                            number1.endsWith('+') ||
+                            number1.endsWith('x') ||
+                            number1.endsWith('-')) {
+                          number2 = number2 + '1';
+                          num2 = double.parse(number2);
+                          counter = double.parse(number2);
+                        } else {
+                          number1 = number1 + '1';
+                          num1 = double.parse(number1);
+                          counter = double.parse(number1);
+                        }
+                        _refresh();
+                      },
                     ),
                   ),
                 ),
@@ -210,7 +413,21 @@ class _loginScreenState extends State<homeScreen> {
                       buttonText: "2",
                       buttonLength: 75,
                       color_button: const Color.fromARGB(42, 255, 255, 255),
-                      onPressedCallback: () {},
+                      onPressedCallback: () {
+                        if (number1.endsWith('÷') ||
+                            number1.endsWith('+') ||
+                            number1.endsWith('x') ||
+                            number1.endsWith('-')) {
+                          number2 = number2 + '2';
+                          num2 = double.parse(number2);
+                          counter = double.parse(number2);
+                        } else {
+                          number1 = number1 + '2';
+                          num1 = double.parse(number1);
+                          counter = double.parse(number1);
+                        }
+                        _refresh();
+                      },
                     ),
                   ),
                 ),
@@ -222,7 +439,21 @@ class _loginScreenState extends State<homeScreen> {
                       buttonText: "3",
                       buttonLength: 75,
                       color_button: const Color.fromARGB(42, 255, 255, 255),
-                      onPressedCallback: () {},
+                      onPressedCallback: () {
+                        if (number1.endsWith('÷') ||
+                            number1.endsWith('+') ||
+                            number1.endsWith('x') ||
+                            number1.endsWith('-')) {
+                          number2 = number2 + '3';
+                          num2 = double.parse(number2);
+                          counter = double.parse(number2);
+                        } else {
+                          number1 = number1 + '3';
+                          num1 = double.parse(number1);
+                          counter = double.parse(number1);
+                        }
+                        _refresh();
+                      },
                     ),
                   ),
                 ),
@@ -234,7 +465,10 @@ class _loginScreenState extends State<homeScreen> {
                       buttonText: "+",
                       buttonLength: 75,
                       color_button: Colors.amber,
-                      onPressedCallback: () {},
+                      onPressedCallback: () {
+                        symbol = '+';
+                        number1 = number1 + symbol;
+                      },
                     ),
                   ),
                 ),
@@ -254,7 +488,21 @@ class _loginScreenState extends State<homeScreen> {
                       buttonText: "0       ",
                       buttonLength: 168,
                       color_button: const Color.fromARGB(42, 255, 255, 255),
-                      onPressedCallback: () {},
+                      onPressedCallback: () {
+                        if (number1.endsWith('÷') ||
+                            number1.endsWith('+') ||
+                            number1.endsWith('x') ||
+                            number1.endsWith('-')) {
+                          number2 = number2 + '0';
+                          num2 = double.parse(number2);
+                          counter = double.parse(number2);
+                        } else {
+                          number1 = number1 + '0';
+                          num1 = double.parse(number1);
+                          counter = double.parse(number1);
+                        }
+                        _refresh();
+                      },
                     ),
                   ),
                 ),
@@ -266,7 +514,21 @@ class _loginScreenState extends State<homeScreen> {
                       buttonText: ".",
                       buttonLength: 75,
                       color_button: const Color.fromARGB(42, 255, 255, 255),
-                      onPressedCallback: () {},
+                      onPressedCallback: () {
+                        if (number1.endsWith('÷') ||
+                            number1.endsWith('+') ||
+                            number1.endsWith('x') ||
+                            number1.endsWith('-')) {
+                          number2 = number2 + '.';
+                          num2 = double.parse(number2);
+                          counter = double.parse(number2);
+                        } else {
+                          number1 = number1 + '.';
+                          num1 = double.parse(number1);
+                          counter = double.parse(number1);
+                        }
+                        _refresh();
+                      },
                     ),
                   ),
                 ),
@@ -278,7 +540,23 @@ class _loginScreenState extends State<homeScreen> {
                       buttonText: "=",
                       buttonLength: 75,
                       color_button: Colors.amber,
-                      onPressedCallback: () {},
+                      onPressedCallback: () {
+                        if (symbol == '÷') {
+                          counter = num1 / num2;
+                        }
+                        if (symbol == '+') {
+                          counter = num1 + num2;
+                        }
+                        if (symbol == '-') {
+                          counter = num1 - num2;
+                        }
+                        if (symbol == 'x') {
+                          counter = num1 * num2;
+                        }
+                        _refresh();
+                        // number1='';
+                        _reset2();
+                      },
                     ),
                   ),
                 ),
